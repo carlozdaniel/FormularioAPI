35d70190fed26ff6a34b7b1e0ae1855684271e47
carlos daniel hernandez <a20160383@gmail.com>
2024-06-28 14:19:28 -0600

    fix error

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,16 @@
-public class User
+
+namespace FormularioAPI.Models
 {
-    public int Id { get; set; }
-    public string? Nombre { get; set; }
-    public string? SegundoNombre { get; set; }
-    public string? ApellidoPaterno { get; set; }
-    public string? ApellidoMaterno { get; set; }
-    public DateTime? FechaNacimiento { get; set; }
-    public string? Email { get; set; }
-    public string? Telefono { get; set; }
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? SegundoNombre { get; set; }
+        public string? ApellidoPaterno { get; set; }
+        public string? ApellidoMaterno { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string? Email { get; set; }
+        public string? Telefono { get; set; }
+    }
 }
+
